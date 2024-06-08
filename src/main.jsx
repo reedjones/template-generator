8c7fab83cbd8ffcdabbee32de8fc42345961a4d7
Reed Jones <reedjones760@yahoo.com>
2024-06-07 17:31:08 -0700

    Update main.jsx
    changed color

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 const colors = {
   brand: {
-    900: "#1a365d",
+    900: "#1b365d",
     800: "#153e75",
     700: "#2a69ac",
   },
