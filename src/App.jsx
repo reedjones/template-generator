510a541d537881504eb30ab692262951695d11ac
reedjones <reedjones760@yahoo.com>
2024-06-07 18:31:36 -0700

    fixed routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import Index from "./pages/Index.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/template-generator/" element={<Index />} />
+        <Route path="/*" element={<Index></Index>}/>
+        <Route path="/template-generator/" element={<Index />} />
       </Routes>
     </Router>
   );
